41ee2bb3cff78252bfc9beada5a4fdf55319b92f
skfada <adamusolomon44@gmail.com>
2024-02-14 14:06:46 +0100

    updating the user link

--- forms.sh
+++ forms.sh
@@ -1,3 +1,55 @@
+#script for executing the database query from bash
+cat ./main/database/schema.sql | mysql -uroot -p
+------------------
+show databases;
+--------------------
+ "for removing carriage retuen spaces in file"
+sed -i 's/\r$//' ./*
+sed -i "s/mysql+mysqldb:/mysql:/" ./main/database/config.py
+-----
+python3 -m pip install flask_bcrypt
+------
+#creating a new enabled file for the instant meal in nginx
+sudo touch /etc/nginx/sites-enabled/instantmeal
+sudo vim /etc/nginx/sites-enabled/instantmeal
+-------------------
+iserver {
+    listen 80;
+    server_name instant-meal.skfada.tech;
+
+    location /static {
+        alias /home/ubuntu/instant_meal/main/static;
+    }
+
+    location / {
+        proxy_pass http://localhost:8000;
+        include /etc/nginx/proxy_params;
+        proxy_redirect off;
+    }
+
+}
+=================
+sudo lsof -i :8000
+
+sudo kill -9 195187
+sudo kill -9 195189
+sudo kill -9 195190
+sudo kill -9 195191
+
+---------------
+number of workers = `nproc -all`* 2 + 1 = 3
+--------
+gunicorn --bind 0.0.0.0:8000 run:app
+
+gunicorn -w 3 run:app
+---
+sudo vim /etc/nginx/nginx.conf
+
+sudo rm /etc/nginx/sites-enabled/instantmeal
+--
+
+
+
 # POST RESGISTRATION TESTING
 name="user4"
 mail="user4@gmail.com"
