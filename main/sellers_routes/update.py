fd8f5f5ec4e336a105fa4de210cd8471d46b58f3
skfada <adamusolomon44@gmail.com>
2024-02-22 23:31:55 +0100

    updated main/sellers_routes/update.py

--- main/sellers_routes/update.py
+++ main/sellers_routes/update.py
@@ -10,7 +10,7 @@ def update():
                 flash(f'please provide entry for "{key}"', 'caution')
                 return redirect(url_for('seller.update'))
 
-        
+
         company_name = request.form['company_name']
         company_reg_no = request.form['company_reg_no']
         date_of_establishment = request.form['date_of_establishment']
@@ -20,7 +20,7 @@ def update():
         State_of_opration = request.form['State_of_opration']
         lga_operation = request.form['lga_operation']
         company_address = request.form['company_address']
-        
+
         '''checking if current user and existing user match'''
         current_user = session['seller_name']
         seller_obj = Sellers.query.filter_by(username=current_user).first()
@@ -29,7 +29,7 @@ def update():
             '''updating the existin record'''
             seller_obj.company_name = company_name
             seller_obj.company_reg_no = company_reg_no
-            seller_obj.date_of_establishment = date_of_establishment
+            seller_obj.date_of_establishment = datetime.strptime(date_of_establishment, '%Y-%m-%d').date()
             seller_obj.occupation = occupation
             seller_obj.company_phone_no = company_phone_no
             seller_obj.country_of_operation = country_of_operation
