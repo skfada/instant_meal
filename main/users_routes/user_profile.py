8cab3948d0a4253e6e44063afff499cea89a4f0b
skfada <adamusolomon44@gmail.com>
2024-02-24 04:49:40 +0100

    updated main/users_routes/user_profile.py

--- main/users_routes/user_profile.py
+++ main/users_routes/user_profile.py
@@ -25,27 +25,33 @@ def update():
 
         '''checking if current user and existing user match'''
         current_user = session['user_name']
-        user_object = Users.query.filter_by(username=current_user).first()
+        try:
+            user_object = Users.query.filter_by(username=current_user).first()
 
-        if  user_object:
-            '''updating the existin record'''
-            user_object.first_name = first_name
-            user_object.last_name = last_name
-            user_object.date_of_birth = datetime.strptime(date_of_birth, '%Y-%m-%d').date()
-            user_object.occupation = occupation
-            user_object.phone_no = phone_no
-            user_object.country_of_residence = country
-            user_object.State_of_residence = state
-            user_object.lga_residence = lga
-            user_object.house_address = house
-            '''commit the changes to reflect in database'''
-            db.session.commit()
+            if  user_object:
+                '''updating the existin record'''
+                user_object.first_name = first_name
+                user_object.last_name = last_name
+                user_object.date_of_birth = datetime.strptime(date_of_birth, '%Y-%m-%d').date()
+                user_object.occupation = occupation
+                user_object.phone_no = phone_no
+                user_object.country_of_residence = country
+                user_object.State_of_residence = state
+                user_object.lga_residence = lga
+                user_object.house_address = house
+                '''commit the changes to reflect in database'''
+                with db.session.begin_nested():
+                    db.session.commit()
 
-            flash('update completed', 'success')
-            return redirect(url_for('user.dashboard'))
+                flash('update completed', 'success')
+                return redirect(url_for('user.dashboard'))
 
-        else:
-            flash('the user does not exist, please confirm details', 'caution')
+            else:
+                flash('the user does not exist, please confirm details', 'caution')
+                return redirect(url_for('user.update'))
+        except Exception:
+            flash(f'there was an error with the server', 'caution')
+            app.logger.error(f'there was an error: {Exception}')
             return redirect(url_for('user.update'))
 
     else:
@@ -56,17 +62,9 @@ def update():
 def password_reset():
     if request.method == "POST" or request.method == "PUT":
         ''' checking for empty form submision '''
-        if request.form["username"] == "":
-            flash(f"Please Enter your User Name", "caution")
-            return redirect(url_for('user.password_reset'))
-        if request.form["email"] == "":
-            flash(f"Please Enter your Email Address", "caution")
-            return redirect(url_for('user.password_reset'))
-        if request.form["pwd1"] == "":
-            flash(f"Please Enter your Password", "caution")
-            return redirect(url_for('user.password_reset'))
-        if request.form["pwd2"] == "":
-            flash(f"Please Enter Confrim Password", "caution")
+        for item in request.form:
+            if request.form[item] == "":
+                flash(f"Please provide information for {item}", "caution")
             return redirect(url_for('user.password_reset'))
 
         ''' checking if password and confirmation password are same '''
@@ -78,22 +76,28 @@ def password_reset():
             return redirect(url_for('user.password_reset'))
 
         ''' checking if user already exist in the database '''
-        user_object = Users.query.filter_by(email=request.form["email"]).first()
+        try:
+            user_object = Users.query.filter_by(email=request.form["email"]).first()
 
-        if user_object and user_object.email == request.form["email"]:
-            hash_pwd = generate_password_hash(request.form["pwd2"]).decode("utf-8")
+            if user_object and user_object.email == request.form["email"]:
+                hash_pwd = generate_password_hash(request.form["pwd2"]).decode("utf-8")
 
-            """ seeting new password """
-            user_object.pwd_hashed = hash_pwd
-            """ setting the user attemt to zero to re-enable access """
-            user_object.login_attempt = 0
-            """ storing user data to the database """
-            db.session.commit()
-            flash("password reset Completed", "success")
-            return redirect(url_for('user.login'))
+                """ seeting new password """
+                user_object.pwd_hashed = hash_pwd
+                """ setting the user attemt to zero to re-enable access """
+                user_object.login_attempt = 0
+                """ storing user data to the database """
+                with db.seeion.begin_nested():
+                    db.session.commit()
+                flash("password reset Completed", "success")
+                return redirect(url_for('user.login'))
 
-        else:
-            flash("User Does not Exist, please register to proceed")
+            else:
+                flash("User Does not Exist, please register to proceed")
+                return redirect(url_for('user.register'))
+        except Exception:
+            flash(f'there was an error with the server', 'caution')
+            app.logger.error(f'there was an error: {Exception}')
             return redirect(url_for('user.register'))
     else:
         return render_template('user/password_reset.html')
@@ -111,36 +115,42 @@ def fund_wallet():
 
 
         ''' comparing loged in user information with database '''
-        user_object = Users.query.filter_by(username=request.form["user_name"]).first()
-        admin_obj =  Users.query.filter_by(username='admin').first()
-        if user_object:
-            amount = int(request.form['amount'])
+        try:
+            user_object = Users.query.filter_by(username=request.form["user_name"]).first()
+            admin_obj =  Users.query.filter_by(username='admin').first()
+            if user_object:
+                amount = int(request.form['amount'])
 
-            ''' DEBIT ADMIN AND CREDIT USER'''
-            admin_obj.wallet = admin_obj.wallet - amount
-            user_object.wallet = user_object.wallet + amount
-            db.session.commit()
+                ''' DEBIT ADMIN AND CREDIT USER'''
+                admin_obj.wallet = admin_obj.wallet - amount
+                user_object.wallet = user_object.wallet + amount
+                db.session.commit()
 
-            ''' RECORD TRANSACTION IN JOURNAL'''
-            new_record = GeneralJournal(
-                sender_username = admin_obj.username,
-                receiver_username = user_object.username,
-                description = f'from: {admin_obj.username} to: {user_object.username}',
-                dr_amnt = amount,
-                cr_amnt = amount,
-                sender_wallet = admin_obj.wallet,
-                receiver_wallet = user_object.wallet
-            )
-            db.session.add(new_record)
-            db.session.commit()
+                ''' RECORD TRANSACTION IN JOURNAL'''
+                new_record = GeneralJournal(
+                    sender_username = admin_obj.username,
+                    receiver_username = user_object.username,
+                    description = f'from: {admin_obj.username} to: {user_object.username}',
+                    dr_amnt = amount,
+                    cr_amnt = amount,
+                    sender_wallet = admin_obj.wallet,
+                    receiver_wallet = user_object.wallet
+                )
+                db.session.add(new_record)
+                db.session.commit()
 
-            session['user_fmt_budget'] = fmtNumber(user_object.wallet)
-            flash(f"Transaction was completed", "info")
-            return redirect(url_for('user.market'))
+                session['user_fmt_budget'] = fmtNumber(user_object.wallet)
+                flash(f"Transaction was completed", "info")
+                return redirect(url_for('user.market'))
 
-        else:
-            flash(f"user does not match for {request.form['user_id']}", "caution")
+            else:
+                flash(f"user does not match for {request.form['user_id']}", "caution")
+                return redirect(url_for('user.fund_wallet'))
+        except Exception:
+            flash(f'there was an error with the server', 'caution')
+            app.logger.error(f'there was an error: {Exception}')
             return redirect(url_for('user.fund_wallet'))
+
     else:
         return render_template('user/user_fundwallet.html')
 
